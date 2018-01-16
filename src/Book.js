bedb89b2e6451b783e01044e1af1084cc6aa2c2e
wt <wt@rmbp2013wt.local>
2018-01-16 22:44:35 +0800

    feature:增加Book组件

---
+++ src/Book.js
@@ -0,0 +1,33 @@
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Book extends Component {
+  static propTypes = {
+    coverURL:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+    authors:PropTypes.array.isRequired
+  }
+  render() {
+    const {coverURL, title, authors} = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${coverURL})` }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors.join(', ')}</div>
+      </div>
+    );
+  }
+}
+
+export default Book;
